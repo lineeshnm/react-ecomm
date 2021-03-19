ba6860317c63ca25ccf9eae9eceba71af5ec0167
Lineesh NM <lineesh2009@gmail.com>
2021-03-19 08:08:08 +0530

    Integrated with firebase, google signin

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyBUVo65t7Cu-Bxn5ejtcRSR9q6-ruWbWqs",
+    authDomain: "prime-script-241521.firebaseapp.com",
+    databaseURL: "https://prime-script-241521.firebaseio.com",
+    projectId: "prime-script-241521",
+    storageBucket: "prime-script-241521.appspot.com",
+    messagingSenderId: "203281726906",
+    appId: "1:203281726906:web:4eca570d506bb343f27183"
+};
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
