70393c6637b0af0929f48be024abd5433eb74f57
Lineesh NM <lineesh2009@gmail.com>
2021-03-22 08:44:07 +0530

    updated cardutils

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,11 +1,18 @@
 import { cartActionTypes } from './cart.types'
+import { addItemtoCart } from './cart.utils'
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case cartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemtoCart(state.cartItems, action.payload)
+            }
         case cartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
