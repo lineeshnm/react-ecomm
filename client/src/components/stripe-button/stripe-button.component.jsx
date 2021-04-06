e985177ba55ddd20157bd0b6a92ed99c372f6a75
Lineesh NM <lineesh2009@gmail.com>
2021-04-06 08:14:10 +0530

    Separating client and server, added express payment route

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100
     const publishableKey = 'pk_test_xdkfjdjfsdjfjgjfjgl;fjglk;hflgjkfjgfjgljf'
 
     const onToken = token => {
-        console.log(token)
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful')
+        }).catch(error => {
+            console.log('Payment Error: ', JSON.parse(error))
+            alert("There was an issue with your payment")
+        })
     }
 
     return (
